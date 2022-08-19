2d7beb32885adf9590a2576ba672ef6c2ad6abe3
Monarch93 <Monarch93@mail.ru>
2022-08-19 20:55:03 +0300

    HW

---
+++ src/main/java/ru/gb/AppConfig.java
@@ -0,0 +1,21 @@
+package ru.gb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@ComponentScan("ru.gb")
+public class AppConfig {
+
+    @Bean
+    public EntityManagerFactory serviceFactory(){ return new ServiceFactory().getEntityManagerFactory(); }
+
+    @Bean
+    public ProductDao productDao(EntityManagerFactory emf) {
+        return new ProductDao(emf);
+    }
+}
